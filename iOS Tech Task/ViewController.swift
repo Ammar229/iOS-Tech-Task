ae8363c27ff2ac152616a27b348600af0dd086bd
Ammar <3mmar.229@gmail.com>
2021-03-24 01:51:40 +0200

    Initial Commit

---
+++ iOS Tech Task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS Tech Task
+//
+//  Created by Ammar on 3/24/21.
+//  Copyright © 2021 Ammar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
